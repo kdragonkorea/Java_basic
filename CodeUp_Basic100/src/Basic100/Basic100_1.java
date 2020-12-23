fc0938fade0ac9c70ad437f71dc539f80792f211
kdragonkorea <morenmore6340@gmail.com>
2020-12-23 18:39:12 +0900

    CodeUp 기초100 문제풀이

---
+++ CodeUp_Basic100/src/Basic100/Basic100_1.java
@@ -0,0 +1,21 @@
+package Basic100;
+
+public class Basic100_1 {
+	public static void main(String[] args) {
+	//문제1. Hello을 출력하세요.
+//	public class Main {
+//		public static void main(String[] args) {
+//			System.out.println("Hello"); 
+	//class 이름을 반드시 'Main'으로 입력해야 정상적으로 출력
+	
+	//문제2. Hello World을 출력하세요.
+//	public class Main {
+//	    public static void main(String[] args) {
+//	        System.out.print("Hello World");
+//	    }
+//	}
+
+	
+
+	}
+}
