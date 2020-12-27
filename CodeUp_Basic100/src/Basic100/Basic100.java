b2948686b7e81bdb374ede76fcfbadd656b2459d
kdragonkorea <morenmore6340@gmail.com>
2020-12-27 18:39:53 +0900

    CodeUp 기초100 문제풀이 - 1015까지

---
+++ CodeUp_Basic100/src/Basic100/Basic100.java
@@ -0,0 +1,132 @@
+package Basic100;
+
+import java.util.Scanner;
+
+public class Basic100 {
+	public static void main(String[] args) {
+//	[문제1001] Hello을 출력하세요.
+//	정답:
+//	public class Main {
+//		public static void main(String[] args) {
+//			System.out.println("Hello"); 
+//	* class 이름을 반드시 'Main'으로 입력해야 정상적으로 출력
+	
+//	[문제1002] Hello World을 출력하세요.
+//	정답: 
+//	public class Main {
+//	    public static void main(String[] args) {
+//	        System.out.print("Hello World");
+//	    }
+//	}
+//	
+//	[문제1003] 아래 문장을 출력하세요.
+		//Hello
+		//World
+//	정답:
+//	public class Main {
+//		public static void main(String[] args) {
+//		    System.out.print("Hello\nWorld");
+//	    }
+//	}
+//
+//	[문제1004] 아래 문장을 출력하세요.
+		//'Hello'
+//	정답:
+//	public class Main {
+//		public static void main(String[] args) {
+//		    System.out.print("\'Hello\'");
+//	    }
+//	}
+//	[문제1005] 아래 문장을 출력하세요.
+		//"Hello World"
+//	정답:
+//	public class Main {
+//	    public static void main(String[] args) {
+//	        System.out.print("\"Hello World\"");
+//	        }
+//	    }	
+//		
+//	[문제1006] 아래 문장을 출력하세요.
+		//"!@#$%^&*()"
+//	정답:
+//	public class Main {
+//		public static void main(String[] args) {
+//			System.out.println("!@#$%^&*()");
+//			}
+//		}
+//	[문제1007] 아래 문장을 출력하세요.
+//	"C:\Download\hello.cpp"
+//	정답:
+//	public class Main {
+//		public static void main(String[] args) {
+//		    System.out.print("\"C:\\Download\\hello.cpp\"");
+//	  	  }
+//		}
+//	[문제1008] 아래 문장을 출력하세요.
+//	┌┬┐
+//	├┼┤
+//	└┴┘
+
+//	정답:
+//	public class Main {
+//		public static void main(String[] args) {
+//		    System.out.println("\u250C\u252C\u2510");
+//			System.out.println("\u251C\u253C\u2524");
+//			System.out.print("\u2514\u2534\u2518");
+
+//	[문제1010] 정수를 입력 후 출력해보세요.
+//	[해결하지 못한 문제]		
+//	
+//	public class Main {
+//		public static void main(String[] args) {
+//	    Scanner sc=new Scanner(System.in);
+//
+//		System.out.println("정수를 입력하세요: ");
+//	    int data=sc.nextInt(); 
+//	    System.out.println(data);
+//	[문제1011] 문자형(char)으로 변수를 하나 선언하고, 변수에 문자를 저장한 후 변수에 저장되어 있는 문자를 그대로 출력해보자.
+//	[정답]
+//	import java.util.Scanner;
+//	public class Main {
+//	    public static void main(String[] args) {
+//	        Scanner sc = new Scanner(System.in);
+//			String x = sc.next();
+//			System.out.println(x);
+//		}
+//	}
+		
+//	[문제1012] 실수형(float)로 변수를 선언하고 그 변수에 실수값을 저장한 후 저장되어 있는 실수값을 출력해보자.
+//	[해결하지 못한 문제]	
+//	import java.util.Scanner;
+//	public class Main {
+//		public static void main(String[] args) {
+//			Scanner sc = new Scanner(System.in);
+//			float x = (float) sc.nextDouble();
+//			System.out.print(x);
+
+		
+//	[문제1013] 정수(int) 2개를 입력받아 그대로 출력해보자.
+//	import java.util.Scanner;
+//	public class Main {
+//		public static void main(String[] args) {
+//	        Scanner sc = new Scanner(System.in);
+//			int x = sc.nextInt();
+//			int y = sc.nextInt();
+//			System.out.println(x);
+//			System.out.println(y);
+//		}
+//	}
+				
+//	[문제1014] 2개의 문자(ASCII CODE)를 입력받아서 순서를 바꿔 출력해보자.
+//	import java.util.Scanner;
+//	public class Main {
+//		public static void main(String[] args) {
+//		    Scanner sc = new Scanner(System.in);
+//			String data1 = sc.next(); 
+//			String data2 = sc.next();
+//			System.out.print(data2+" "+data1);
+//		}
+//	}
+	}
+	
+}
